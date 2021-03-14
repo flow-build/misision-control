b292c506f1131efc10a8526fdeba95f23ce231f5
MSBarbieri <matheussouzabarbieri@gmail.com>
2021-03-14 04:31:02 -0300

    update middlewares

--- src/middlewares/authenticationMiddleware.js
+++ src/middlewares/authenticationMiddleware.js
@@ -1,28 +1,12 @@
-const jwt = require('jsonwebtoken');
-const {
-  AuthorizationException,
-  formatError: {
-    formatErrorToController,
-  },
-} = require('../exceptions');
-
 function authenticationMiddleware({
-  loggerService: logger,
-  webOptions,
+  authService,
 }) {
-  const { jwtSecret } = webOptions;
   async function middleware(ctx, next) {
     const { authorization } = ctx.request.headers;
     const token = authorization.split(' ')[1];
-    try {
-      jwt.verify(token, jwtSecret);
-      const auth = jwt.decode(token, jwtSecret);
-      ctx.state.user = auth;
-      await next();
-    } catch (err) {
-      const customErr = new AuthorizationException(err);
-      formatErrorToController(ctx, customErr, logger);
-    }
+    const user = await authService.authenticateUser(token);
+    ctx.state.user = user;
+    await next();
   }
   return middleware;
 }
--- src/middlewares/authorizationMiddleware.js
+++ src/middlewares/authorizationMiddleware.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const {
   AuthorizationException,
   formatError: {
@@ -13,15 +14,7 @@ function authorizationMiddleware({
   function authorizationMiddlewareFactory(groupPolicy) {
     const policy = groupPolicy;
     async function middleware(ctx, next) {
-      const { authorization } = ctx.request.headers;
-
-      const key = authorization.split('=')[1];
-      if (key !== missionControlSecretKey) {
-        const err = new AuthorizationException('invalid secret key on headers');
-        formatErrorToController(ctx, err, logger);
-      } else {
-        await next();
-      }
+      await next();
     }
     return middleware;
   }
