a09123abf3755481a1c5ffa43a526b46de801e87
MSBarbieri <matheussouzabarbieri@gmail.com>
2021-03-24 11:45:00 -0300

    migration

--- db/seeds/organization.js
+++ db/seeds/organization.js
@@ -1,12 +1,11 @@
 const { v4 } = require('uuid');
 
 exports.seed = async (knex) => {
-  // Deletes ALL existing entries
   const defaultOrganization = {
     id: v4(),
     name: 'flowbuild',
     secret_key: process.env.FLOWBUILD_SECRET_KEY,
-    url: 'http://localhost:3100',
+    url: 'observer:3100',
     created_at: new Date(),
     updated_at: new Date(),
   };
